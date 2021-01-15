c29d526d772b9d4acc30635e6d6eddd7624ebe7e
Amjadalrefayi <69738151+Amjadalrefayi@users.noreply.github.com>
2021-01-15 18:04:03 +0200

    Add function to Delete a comment Add function to edit a comment

--- backend app/app/src/main/java/com/company/app/Controllers/CommentController.java
+++ backend app/app/src/main/java/com/company/app/Controllers/CommentController.java
@@ -6,7 +6,6 @@ import com.company.app.Repositories.UserRepo;
 import com.company.app.entities.CommentEntity;
 import com.company.app.entities.PostEntity;
 import com.company.app.entities.UserEntity;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,8 +27,7 @@ public class CommentController {
     public Boolean addComment(@RequestBody String value, @PathVariable(name = "u_id") Long u_id,
             @PathVariable(name = "p_id") Long p_id) {
         CommentEntity comment = new CommentEntity();
-         if(userRepo.findById(u_id).isPresent() && postRepo.findById(p_id).isPresent())
-        {
+        if (userRepo.findById(u_id).isPresent() && postRepo.findById(p_id).isPresent()) {
             UserEntity u = userRepo.findById(u_id).get();
             PostEntity p = postRepo.findById(p_id).get();
             comment.setUEntity(u);
@@ -37,10 +35,46 @@ public class CommentController {
             comment.setValue(value);
             comment = commentRepo.save(comment);
             return commentRepo.findById(comment.getId()).isPresent();
-        }else
-        { 
+        } else {
             return false;
         }
-       
     }
+
+    @PostMapping(path = "/delete/{u_id}/{p_id}/{c_id}")
+    public Boolean deleteComment(@PathVariable(name = "u_id") Long u_id, @PathVariable(name = "p_id") Long p_id,
+            @PathVariable(name = "c_id") Long c_id) {
+        if (!userRepo.findById(u_id).isPresent() || !commentRepo.findById(c_id).isPresent()
+                || !postRepo.findById(p_id).isPresent())
+            return false;
+
+        CommentEntity commentEntity = commentRepo.findById(c_id).get();
+        UserEntity userEntity = userRepo.findById(u_id).get();
+
+        if (userEntity.getComments().contains(commentRepo.findById(c_id).get())) {
+            commentRepo.delete(commentEntity);
+            return true;
+        } else
+            return false;
+    }
+
+    @PostMapping(path = "/edit/{u_id}/{p_id}/{c_id}")
+    public Boolean editComment(@RequestBody String value, @PathVariable(name = "u_id") Long u_id,
+            @PathVariable(name = "p_id") Long p_id, @PathVariable(name = "c_id") Long c_id) {
+        UserEntity userEntity = userRepo.findById(u_id).get();
+        if (!userRepo.findById(u_id).isPresent() || !commentRepo.findById(c_id).isPresent()
+                || !postRepo.findById(p_id).isPresent())
+            return false;
+        else {
+            if (userEntity.getComments().contains(commentRepo.findById(c_id).get())) {
+                CommentEntity comment = commentRepo.findById(c_id).get();
+                comment.setValue(value);
+                comment = commentRepo.save(comment);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
 }
